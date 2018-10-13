8deafb7e153997a277aeadb58de048c93218ac89
Martina <martts00@gmail.com>
2018-10-14 00:39:30 +0300

    initial commit

---
+++ Zadacha_2_OddNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha_2_OddNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int chislo;
+
+            Console.WriteLine("Please insert a number");
+            chislo = int.Parse(Console.ReadLine());
+
+            if (chislo % 2 == 0)
+            {
+                Console.WriteLine("Chetno e!");
+            }
+            else
+            {
+                Console.WriteLine("Nechetno e!");
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
